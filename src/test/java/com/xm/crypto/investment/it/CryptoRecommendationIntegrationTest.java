1a9f14504095894c1b32e23cd5b427a527256058
Sherzod Nurjonov <jd.sherzod@gmail.com>
2023-03-03 11:58:26 +0500

    integration tests added to increase test coverage

--- src/test/java/com/xm/crypto/investment/it/CryptoRecommendationIntegrationTest.java
+++ src/test/java/com/xm/crypto/investment/it/CryptoRecommendationIntegrationTest.java
@@ -1,10 +1,14 @@
 package com.xm.crypto.investment.it;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.xm.crypto.investment.domain.dto.CryptoNormalizedDto;
+import com.xm.crypto.investment.domain.dto.CryptoNormalizedListDto;
 import com.xm.crypto.investment.domain.dto.CryptoStatisticDto;
 import com.xm.crypto.investment.service.CryptoDataService;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,7 +20,7 @@ public class CryptoRecommendationIntegrationTest extends AbstractIntegrationTest
   private RedisTemplate<String, Object> redisTemplate;
 
   @Test
-   public void testGetStatisticsByCrypto_BTC_allInterval() {
+  public void testGetStatisticsByCrypto_BTC_allInterval() {
     var resultFromDb = cryptoDataService.getStatisticsByCrypto("BTC", null, null);
     assertEquals(BigDecimal.valueOf(46813.21), resultFromDb.getOldest());
 
@@ -35,5 +39,19 @@ public class CryptoRecommendationIntegrationTest extends AbstractIntegrationTest
     assertEquals("BTC", result.getCryptos().get(1).getSymbol());
     assertEquals(BigDecimal.valueOf(0.43), result.getCryptos().get(1).getNormalizedPrice());
   }
+
+  @Test
+  public void testGetNormalizedRangeHighest_forSpecificDay() {
+    var date = LocalDate.of(2022, 1, 1);
+    var result = cryptoDataService.getNormalizedRangeHighest(date);
+    assertEquals("BTC", result.getSymbol());
+    assertEquals(BigDecimal.valueOf(0.02), result.getNormalizedPrice());
+    var key = String.format("normalized_%s_%s", date, date.plusDays(1));
+    var cachedValue = (CryptoNormalizedListDto) redisTemplate.opsForValue().get(key);
+    assertNotNull(cachedValue);
+    assertEquals(cachedValue.getCryptos().get(0), result);
+  }
+
+  //some other cases need to be covered, my time is over for this task, so that I need to push my changes here
 
 }
